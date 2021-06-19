bb976be92a86257f93ef233f9e42680cc573a934
Juho Päivärinta <juho.paivarinta@outlook.com>
2021-06-19 16:05:31 +0300

    Add infra folder.

---
+++ infra/kubernetes/variables.tf
@@ -0,0 +1,45 @@
+variable "env" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "k8s_master_allowed_ip" {
+  type = string
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+  type    = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "node_count" {
+  default = "1"
+  type    = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "container_registry_name" {
+  type = string
+}
+
+variable "gke_cluster_name" {
+  type = string
+}
+
+variable "container_node_pool_name" {
+  type = string
+}
